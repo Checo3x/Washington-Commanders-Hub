200d5784263b1801c07cc176e30659bc5ced9055
Sergio Martín Calero <101972076+Checo3x@users.noreply.github.com>
2025-05-03 20:18:32 +0200

    Update espn-events.js

--- api/espn-events.js
+++ api/espn-events.js
@@ -1,14 +1,16 @@
-const fetch = require('node-fetch');
+const API_KEY = process.env.ESPN_API_KEY;
+const TEAM_ID = '28'; // ID de Washington Commanders
 
-module.exports = async (req, res) => {
-    try {
-        const response = await fetch('http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/28/schedule?season=2024');
-        if (!response.ok) {
-            throw new Error('Error al obtener los datos de ESPN');
-        }
-        const data = await response.json();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener los datos de ESPN' });
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(`http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams/${TEAM_ID}/schedule?seasontype=2&groups=all&limit=500&apikey=${API_KEY}`);
+    if (!response.ok) {
+      throw new Error(`Error en la petición a la API de ESPN: ${response.status}`);
     }
-};
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching ESPN events:', error);
+    res.status(500).json({ error: 'Failed to fetch ESPN events' });
+  }
+}
